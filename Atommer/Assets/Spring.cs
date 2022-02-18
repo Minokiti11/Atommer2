8b7d0f84c1f5c34740d21395fdf9f9e52c0c2cf1
Minokiti11 <macminosp@icloud.com>
2022-02-18 21:36:29 +0900

    UpdateScene

---
+++ Atommer/Assets/Spring.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
